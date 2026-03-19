4ccfc8a15426d7a0dc6d7cfbc7c483ed143410fa
rocketman510 <rocketman18354@gmail.com>
2026-03-19 14:59:33 -0700

    Made it so that when the update script is ran it will run [0m[1mbun install [0m[2mv1.3.10 (30e609e0)[0m
    [0m[32mDone[0m! Checked 127 packages[0m [2m(no changes)[0m [0m[2m[[1m13.00ms[0m[2m][0m[0m. This fixes a bug that sill crash the bun instance when the two package.json file don't line up

--- update.sh
+++ update.sh
@@ -5,5 +5,6 @@ git fetch --tags --force https://$TOKEN@github.com/rocketman510/Robo-Pope.git
 latest=$(git describe --tags --abbrev=0)
 git checkout "$latest"
 git pull https://$TOKEN@github.com/rocketman510/Robo-Pope.git main
+bun install
 
 pm2 restart index.ts
