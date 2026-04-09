c557751b79208e470aa2a9ff16d979202d52d71b
rocketman510 <rocketman18354@gmail.com>
2026-04-08 19:40:31 -0700

    Done

--- commands/vowofsilence.ts
+++ commands/vowofsilence.ts
@@ -43,7 +43,7 @@ export default {
     const match_form_back = time_string?.matchAll(/(\d+)\s*(?:[mM][iI][lL][lL][iI][sS][eE][cC][oO][nN][dD][sS]?|[mM][sS]|[mM][iI][lL][lL][sS]?)(?![a-zA-Z])|(\d+)\s*(?:[sS][eE][cC][oO][nN][dD][sS]?|[sS]|[sS][eE][cC][sS]?)(?![a-zA-Z])|(\d+)\s*(?:[mM][iI][nN][uU][tT][eE][sS]?|m|[mM][iI][nN][sS]?)(?![a-zA-Z])|(\d+)\s*(?:[hH][oO][uU][rR][sS]?|[hH])(?![a-zA-Z])|(\d+)\s*(?:[dD][aA][yY][sS]?|[dD])(?![a-zA-Z])|(\d+)\s*(?:[wW][eE][eE][kK][sS]?|[wW])(?![a-zA-Z])|(\d+)\s*(?:[mM][oO][nN][tT][hH][sS]?|M)(?![a-zA-Z])|(\d+)\s*(?:[yY][eE][aA][rR][sS]?|[yY])(?![a-zA-Z])/g);
     const match_form_front = time_string?.matchAll(/(?:[mM][iI][lL][lL][iI][sS][eE][cC][oO][nN][dD][sS]?|[mM][sS]|[mM][iI][lL][lL][sS]?)(?![a-zA-Z]):\s*(\d+)|(?:[sS][eE][cC][oO][nN][dD][sS]?|[sS]|[sS][eE][cC][sS]?)(?![a-zA-Z]):\s*(\d+)|(?:[mM][iI][nN][uU][tT][eE][sS]?|m|[mM][iI][nN][sS]?)(?![a-zA-Z]):\s*(\d+)|(?:[hH][oO][uU][rR][sS]?|[hH])(?![a-zA-Z]):\s*(\d+)|(?:[dD][aA][yY][sS]?|[dD])(?![a-zA-Z]):\s*(\d+)|(?:[wW][eE][eE][kK][sS]?|[wW])(?![a-zA-Z]):\s*(\d+)|(?:[mM][oO][nN][tT][hH][sS]?|M)(?![a-zA-Z]):\s*(\d+)|(?:[yY][eE][aA][rR][sS]?|[yY])(?![a-zA-Z]):\s*(\d+)/g);
 
-    let time: number | null = 0
+    let time: number | null = 0;
 
     for (const matchs of match_form_back.toArray()) {
       for (const [i, value] of matchs.entries()) {
@@ -51,12 +51,26 @@ export default {
         const unit = parseInt(value, 10)
         if (i == 1) time = unit
         if (i == 2) time = unit*1000
-        if (i == 3) time = unit*6000
+        if (i == 3) time = unit*60000
         if (i == 4) time = unit*3600000
         if (i == 5) time = unit*86400000
         if (i == 6) time = unit*604800000
         if (i == 7) time = unit*2592000000
         if (i == 8) time = unit*31536000000
+      }
+    }
+    for (const matchs of match_form_front.toArray()) {
+      for (const [i, value] of matchs.entries()) {
+        if (!value) continue;
+        const unit = parseInt(value, 10)
+        if (i == 1) time = unit
+        if (i == 2) time = unit * 1000
+        if (i == 3) time = unit * 60000
+        if (i == 4) time = unit * 3600000
+        if (i == 5) time = unit * 86400000
+        if (i == 6) time = unit * 604800000
+        if (i == 7) time = unit * 2592000000
+        if (i == 8) time = unit * 31536000000
       }
     }
 
@@ -66,9 +80,10 @@ export default {
       time = Math.min(time, 2_419_200_000)
     }
 
-    if (!member.moderatable) return await interaction.reply({ content: `You can not Timeout this user`, flags: MessageFlags.Ephemeral })
+    if (!member.moderatable) return await interaction.reply({ content: `You can not Timeout this user`, flags: MessageFlags.Ephemeral });
+    const reason = interaction.options.getString('reason') ?? undefined;
 
-    const is_error = await member.timeout(time)
+    const is_error = await member.timeout(time, reason)
       .then(() => {return false;})
       .catch(
         (e) => {
@@ -78,14 +93,14 @@ export default {
       )
     if (is_error) return;
 
-    const timeout_timestamp = Math.floor(member.communicationDisabledUntilTimestamp!/1000)
+    const timeout_timestamp = Math.floor(member.communicationDisabledUntilTimestamp!/1000);
 
     if (time === null) {
       await interaction.reply({ content: `<@${user.id}> can speek now.`})
     } else if (time > max_timeout_time) {
-      await interaction.reply({ content: `<@${user.id}> has decided to take a vow of silence until <t:${timeout_timestamp}:D>\n-# :warning: You can not time out a user for more that \`28 Days\``})
+      await interaction.reply({ content: `<@${user.id}> has decided to take a vow of silence until <t:${timeout_timestamp}:D>\n-# :warning: You can not time out a user for more that \`28 Days\`${reason ? ' for '+reason:''}`})
     } else {
-      await interaction.reply({ content: `<@${user.id}> has decided to take a vow of silence until <t:${timeout_timestamp}:D>\n` })
+      await interaction.reply({ content: `<@${user.id}> has decided to take a vow of silence until <t:${timeout_timestamp}:D>\n${reason ? ' for '+reason:''}` })
     }
   },
 } as Command;
