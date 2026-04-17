e20b9e4d52680973290234ec93a570ea284628d2
rocketman510 <rocketman18354@gmail.com>
2026-04-17 11:40:19 -0700

    Added: Functionality

--- functions/dyn_voice_channel.ts
+++ functions/dyn_voice_channel.ts
@@ -3,8 +3,29 @@ import { ensure } from "..";
 
 export async function handle_join(oldState: VoiceState, newState: VoiceState) {
   const dyn_id = JSON.parse(ensure(process.env.DYNAMIC_VOICE_CHANNELS, "No DYNAMIC_VOICE_CHANNELS ENV"));
-  if (dyn_id.includes(newState.channelId)) {
-    const new_channle = await newState.guild.channels.create({name: `VC:`, type: ChannelType.GuildVoice})
-    newState.member?.voice.setChannel(new_channle)
+  const client = newState.client;
+
+  let channels = client.dyn_vc.ensure(newState.guild.id, () => []);
+
+  if (oldState.channelId && channels.includes(oldState.channelId)) {
+    if (oldState.channel?.members.size == 0) {
+      await oldState.channel.delete()
+      channels = client.dyn_vc.get(oldState.guild.id)!;
+      console.log(channels, oldState.channelId);
+      channels = channels.filter(e => e != oldState.channelId);
+      console.log(channels);
+      client.dyn_vc.set(oldState.guild.id, channels)
+    }
+  }
+
+  channels = client.dyn_vc.get(oldState.guild.id)!;
+
+  if (newState.channelId && dyn_id.includes(newState.channelId)) {
+    const new_channel = await newState.guild.channels.create({name: `VC: ${channels.length + 1}`, type: ChannelType.GuildVoice, parent: newState.channel?.parent});
+    newState.member?.voice.setChannel(new_channel);
+
+    channels = client.dyn_vc.get(oldState.guild.id)!;
+    channels.push(new_channel.id);
+    client.dyn_vc.set(newState.guild.id, channels)
   }
 }
